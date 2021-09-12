f6a27238eda3ed5d407d04142e8085584f187b42
Ethanscript <53057751+Ethanscript@users.noreply.github.com>
2021-09-12 20:16:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,3 @@ int main(void) {
 	compiler* new_compiler = new compiler();
 	new_compiler->start();
 }
-
-/*
-#include "compiler.h"
-
-int main(void) {
-	compiler* new_compiler = new compiler();
-	new_compiler->start();
-}
-*/
